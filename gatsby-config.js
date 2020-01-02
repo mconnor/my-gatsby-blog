233e34d9df3dfb06b979fe09e67fde86a577e32a
mike connor <mike@rocketnumber9.org>
2020-01-02 14:37:25 -0600

    add netflify plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,8 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     'gatsby-plugin-offline',
-    'gatsby-transformer-remark'
+    'gatsby-transformer-remark',
+    'gatsby-plugin-netlify-cms',
+    `gatsby-plugin-netlify`, // make sure to put last in the array
   ],
 }
